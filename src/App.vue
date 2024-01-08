4580bd3f23b7e9f348bbd1941faa5cf512aa242c
jacopobiagi <jacopo.biagi02@gmail.com>
2024-01-08 21:01:16 +0100

    config: inserimento struttura del progetto

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,47 @@
 <script>
+import axios from "axios";
+import { store } from "./store";
 
+import appHeader from './components/appHeader.vue';
+import appMain from './components/appMain.vue';
+import appFooter from './components/appFooter.vue';
+
+export default{
+  components:{
+    appHeader,
+    appMain,
+    appFooter,
+  },
+  data(){
+    return{
+      store,
+    }
+  },
+  methods:{
+    getCharacters(){
+      axios 
+        .get(store.apiURL);
+    }
+  }
+}
 </script>
-
-<template>
   
+<template>
+  <section>
+    </appHeader>
+  </section>
+
+  <section>
+    </appMain>
+  </section>
+
+  <section>
+    </appFooter>
+  </section>
 </template>
 
 <style lang="scss">
 
-  @use './style/general.scss'
+  @use './style/general.scss';
   
 </style>
