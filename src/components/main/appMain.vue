e65c8db78aa76bdb376900cba6c08dd0fbc4507d
jacopobiagi <jacopo.biagi02@gmail.com>
2024-01-09 18:46:56 +0100

    feat: ottimizzazione interfaccia

--- src/components/main/appMain.vue
+++ src/components/main/appMain.vue
@@ -18,18 +18,24 @@ export default {
 
 <template>
 
-    <main>
-        <div>
-
+    <main class="d-flex align-items-center justify-content-center">
+        <select name="" id=""></select>
+        <div class="container-list ">
+            <div class="header-list w-100 text-white bg-dark d-flex justify-content-arount align-items-center">
+                <h2 class="p-3">Found 20 cards</h2>
+            </div>
+            <div class="main-list row">
+                <div v-for="element in store.cardList" class="container-card col-3">
+                    <singleCard
+                    :name="element.name"
+                    :type="element.type"
+                    :img="element.card_images[0].image_url"/>
+                </div>
+            </div>
         </div>
 
-        
-        <div v-for="element in store.cardList">
-            <singleCard
-            :name="element.name"
-            :type="element.type"
-            :img="element.card_images[0].image_url"/>
-        </div>
+
+        <!--  -->
     </main>
     
 </template>
@@ -37,6 +43,26 @@ export default {
 <style lang="scss" scoped>
     main{
         width: 100vw;
+        min-height: calc(100vh - 70px);
         background-color: rgb(202, 137, 55);
+
+        .container-list{
+            max-width: 80%;
+            min-height: 500px;
+            background-color: white;
+            padding: 30px;
+            margin: 40px 0;
+            .header-list{
+                height: 60px;
+                h2{
+                    font-size: 1.2em;
+                }
+            }
+            .container-card{
+                background-color: rgb(255, 255, 255);
+                padding: 11px;
+            }
+            
+        }
     }
 </style>
--- src/components/main/subMain/singleCard.vue
+++ src/components/main/subMain/singleCard.vue
@@ -8,17 +8,29 @@ export default{
 }
 </script>
 <template>
-    <div class="d-flex flex-column">
-        <h2>{{ name }}</h2>
-        <div>
-            <img :src="img" :alt="img">
+    <div class="body-card d-flex flex-column w-100">
+        <div class="img-card">
+            <img :src="img" :alt="img" class="w-100">
         </div>
-        <p>
+        <h2 class="name-card w-100 text-white">{{ name }}</h2>
+        <p class="type-card w-100">
             {{ type }}
         </p>
     </div>
 
 </template>
-<style>
+<style lang="scss" scoped>
+    .body-card{
+        background-color: rgb(202, 137, 55);
+        height: 100%;
 
+        h2{
+            font-size: 1.3em;
+            text-align: center;
+            margin-top: 5px;
+        }
+        p{
+            text-align: center;
+        }
+    }
 </style>
